4b9a8a69f0deac1e437da8487533f85e3f372836
Brendan Fahey <befahey1@my.scccd.edu>
2024-01-17 22:21:06 -0800

    coding is done

---
+++ w2/main.js
@@ -0,0 +1,12 @@
+const myScore = document.querySelector("h2");
+const myScore1 = 6;
+const myScore2 = 4;
+const myScore3 = 8;
+const myScore4 = 5;
+const myScore5 = 4;
+const myScore6 = 20;
+const myScore7 = 8;
+const myScore8 = 6;
+const total = myScore1 + myScore2 + myScore3 + myScore4 + myScore5 + myScore6 + myScore7 + myScore8;
+console.log(total);
+myScore.textContent = total
