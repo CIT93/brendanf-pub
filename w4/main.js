8116461ce47a3951feb54f3ccbd9ab3605c43731
Brendan Fahey <befahey1@my.scccd.edu>
2024-01-31 21:22:38 -0800

    updated output to DOM

--- w4/main.js
+++ w4/main.js
@@ -8,7 +8,7 @@ function determineHouseSizePts(size) {
     houseSizePoints = 7;
   } else if (size === "small") {
     houseSizePoints = 4;
-  } else if (size === "apt") {
+  } else if (size === "apartment") {
     houseSizePoints = 2;
   }
   return houseSizePoints;
@@ -45,19 +45,22 @@ function displayOutput() {
   for (arr of cfpData){
     console.log(arr);
     const output = document.getElementById("output");
+    const newH1 = document.createElement("h1");
+    newH1.textContent = `Carbon Footprint total score is ${arr[4]}`;
     const newP = document.createElement("p");
-    newP.textContent = `Carbon Footprint total is ${arr[4]}`;
+    newP.textContent = `This Carbon Footprint total score number is based the ${arr[0]} total number of people in the household (with the Carbon Footprint household score of ${arr[2]}) living in a(n) ${arr[1]} sized home (with the Carbon Footprint home size score of ${arr[3]}). The Carbon Footprint household and home size scores total to ${arr[4]}.`;
+    output.appendChild(newH1);
     output.appendChild(newP);
   }
 }
 
-start(7, "apt");
-start(6, "apt");
-start(5, "apt");
-start(4, "apt");
-start(3, "apt");
-start(2, "apt");
-start(1, "apt");
+start(7, "apartment");
+start(6, "apartment");
+start(5, "apartment");
+start(4, "apartment");
+start(3, "apartment");
+start(2, "apartment");
+start(1, "apartment");
 start(7, "small");
 start(6, "small");
 start(5, "small");
